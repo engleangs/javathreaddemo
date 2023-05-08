af98f292d75df4e975e790825fc7496e7662591c
saktrapz <saktrapz@gmail.com>
2023-05-08 20:22:15 +0700

    Update Hi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -82,6 +82,7 @@ public class Main {
     public static void main(String[] args) throws SQLException, IOException {
         //writeData();
         System.out.println("Hello world....");
+        System.out.println("Hi");
 
 
     }
